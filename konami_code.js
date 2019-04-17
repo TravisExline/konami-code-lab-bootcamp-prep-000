a1ab893beb11538d67a766f96b6d55363d1a9248
Travis Exline <t.exline22@gmail.com>
2019-04-17 13:19:45 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,16 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+let index = 0;
+document.body.addEventListener('keydown', function init(e) {
+  const key = e.key;
+  if (key === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert("Hurray!");
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+})
